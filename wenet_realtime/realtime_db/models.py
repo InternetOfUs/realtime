b433282a1d9ea1fab5f9fada5b43eb80c9b802e7
William Droz <william.droz@idiap.ch>
2020-07-17 13:13:38 +0200

    use default timestamp

--- wenet_realtime/realtime_db/models.py
+++ wenet_realtime/realtime_db/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, TIMESTAMP, Float
 from sqlalchemy.orm import relationship
+from time import time
 
 from wenet_realtime.realtime_db.database import Base
 
@@ -8,7 +9,7 @@ class UserLocation(Base):
     __tablename__ = "users_locations"
 
     id = Column(String, primary_key=True, index=True)
-    timestamp = Column(TIMESTAMP)
+    timestamp = Column(Integer, default=int(time()))
     latitude = Column(Float)
     longitude = Column(Float)
     accuracy = Column(Integer, default=0)
