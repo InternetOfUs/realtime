593ea0761e8a388ebe41e8a1dc904085d9c9478b
William Droz <william.droz@idiap.ch>
2020-07-17 14:32:03 +0200

    basic setup file

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as f:
+    all_lines = f.read().split("\n")
+    requirements = [line for line in all_lines if "@" not in line]
+    requirements_git = [
+        line.replace("-e", line.split("=")[-1] + " @")
+        for line in all_lines
+        if "@" in line
+    ]
+    requirements += requirements_git
+
+setuptools.setup(
+    name="wenet_realtime",
+    version="0.0.1",
+    author="Idiap - William Droz",
+    author_email="william.droz@idiap.ch",
+    description="Realtime APIs for the Wenet project (personal context builder component)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=["Programming Language :: Python :: 3"],
+    install_requires=requirements,
+)
