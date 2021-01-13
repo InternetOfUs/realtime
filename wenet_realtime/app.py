425c6b94d88d9edccb105d2ec49d063e1a919257
William Droz <william.droz@idiap.ch>
2021-01-13 08:25:54 +0100

    new post route to get the location of the users

--- wenet_realtime/app.py
+++ wenet_realtime/app.py
@@ -25,6 +25,13 @@ def create_user(user: schemas.UserLocation, db: Session = Depends(get_db)):
     crud.create_or_update(db, user)
 
 
+@app.post("/locations/")
+def get_locations(user_list: schemas.UsersList, db: Session = Depends(get_db)):
+    user_ids = [user_id for user_id in user_list.userids]
+    user_locations = crud.get_locations(db, user_ids)
+    return user_locations
+
+
 @app.get("/closest/")
 def closest_users(
     latitude: float,
