cf314fa1f395acccdd6cc65fbe4e3eb58bc5ee0f
William Droz <william.droz@idiap.ch>
2021-01-26 15:17:54 +0100

    add module for logging

---
+++ wenet_realtime/wenet_logging.py
@@ -0,0 +1,30 @@
+from wenet_realtime import config
+import logging
+import logging.handlers
+
+def create_ch_handler():
+    formatter = logging.Formatter(config.DEFAULT_LOGGER_FORMAT)
+    ch = logging.StreamHandler()
+    ch.formatter = formatter
+    return ch
+
+
+def create_log_file_handler():
+    formatter = logging.Formatter(config.DEFAULT_LOGGER_FORMAT)
+    log_file = logging.handlers.WatchedFileHandler(config.DEFAULT_LOG_FILE)
+    log_file.formatter = formatter
+    return log_file
+
+
+def create_logger(name="wenet-undefined"):
+    """ create a logger with the correct configuration
+    """
+
+    logger = logging.getLogger(name)
+    logger.setLevel(config.DEFAULT_LOGGER_LEVEL)
+    ch = create_ch_handler()
+    log_file = create_log_file_handler()
+
+    logger.addHandler(log_file)
+    logger.addHandler(ch)
+    return logger
