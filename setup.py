0baa62ac5a358224c954f81103163571f47dac92
William DROZ <william.droz@idiap.ch>
2022-05-12 10:41:16 +0200

    bump to v1.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("requirements.txt", "r") as f:
 
 setuptools.setup(
     name="wenet_realtime",
-    version="1.0.0",
+    version="1.1.0",
     author="Idiap - William Droz",
     author_email="william.droz@idiap.ch",
     description="Realtime APIs for the Wenet project (personal context builder component)",
