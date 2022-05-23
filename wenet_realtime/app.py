9774c0cec73927199e14f0c0ed4614ad1c6e01da
William DROZ <william.droz@idiap.ch>
2022-05-23 15:13:53 +0200

    modify root_path only when not in env

--- wenet_realtime/app.py
+++ wenet_realtime/app.py
@@ -13,7 +13,10 @@ from wenet_realtime.wenet_logging import create_ch_handler, create_log_file_hand
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(root_path="/" if config.DEFAULT_ENV == "dev" else config.DEFAULT_NONDEV_PREFIX)
+if config.DEFAULT_ENV == "env":
+    app = FastAPI()
+else:
+    app = FastAPI(root_path=config.DEFAULT_NONDEV_PREFIX)
 
 
 # Dependency
